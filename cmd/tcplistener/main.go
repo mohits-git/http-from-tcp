0dac1cdab4f98cebbb92c90bc04f6bb9d6056a30
mohits-git <152606488+mohits-git@users.noreply.github.com>
2025-03-20 23:38:13 +0530

    wire up, print headers and body for incoming requests

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -32,9 +32,11 @@ func main() {
 		fmt.Printf(" - Method: %s\n", req.RequestLine.Method)
 		fmt.Printf(" - Target: %s\n", req.RequestLine.RequestTarget)
 		fmt.Printf(" - Version: %s\n", req.RequestLine.HttpVersion)
-
-		// TODO: conn close?
-		conn.Close()
-		fmt.Println("Connection Closed.")
+		fmt.Printf("Headers: \n")
+		for k, v := range req.Headers {
+			fmt.Printf(" - %s: %s\n", k, v)
+		}
+		fmt.Printf("Body: \n")
+		fmt.Println(string(req.Body))
 	}
 }
